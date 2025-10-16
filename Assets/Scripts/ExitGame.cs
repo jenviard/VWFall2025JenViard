aeafa89756316b7c0a569fee50730fb9e26a65c9
jennaviard <jiv19@miami.edu>
2025-10-16 01:31:33 -0400

    change panel

--- Assets/Scripts/ExitGame.cs
+++ Assets/Scripts/ExitGame.cs
@@ -4,12 +4,10 @@ public class ExitGame : MonoBehaviour
 {
     public void QuitGame()
     {
-        // This will quit the application when built
         Application.Quit();
 
-        // This is only for the Unity editor (so you can test)
-#if UNITY_EDITOR
+    #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-#endif
+    #endif
     }
 }
